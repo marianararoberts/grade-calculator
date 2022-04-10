63239b98e59e860df822cb190dea8877746421c8
Your Name <you@example.com>
2022-04-10 15:26:59 -0400

    slowly but surely

---
+++ script.js
@@ -0,0 +1,6 @@
+var btn = document.getElementById("nextBtn");
+var cHeader = document.getElementById("classHeader");
+btn.addEventListener("click", function () {
+    let className = document.getElementById("className").value;
+});
+                     
